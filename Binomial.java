8b0859c3b61c8dab9b02477a9d0c85b3aed3900c
Mark Melikov <markmelicov@gmail.com>
2025-12-31 22:36:26 +0200

    https://github.com/cs-intro-reichman/hw7-1-Sokol1001.git

--- Binomial.java
+++ Binomial.java
@@ -4,7 +4,7 @@ public class Binomial {
 		//// Uncomment the version of binomial that you want to test
  
 		// Testing the basic binomial implementation:
-    	// System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    	System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 
 		// Testing the optimized binomial implementation:
 		// System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
@@ -12,17 +12,21 @@ public class Binomial {
 
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
-		//// Repplace the following comment with your code
-		return 0;
+		if (k > n) return 0;
+        if (k == 0 || n == k || n == 0) return 1;
+        return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	 }
 	
 	// Computes the Binomial function, efficiently
 	public static int binomial(int n, int k) {
-		//// This function creates a 2D array, say memo, 
-		//// and then initializes all its elements to -1.
-		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
-		//// Replace the following statement with your code.
-		return 0;
+        if (k > n) return 0;
+		int[][] memo = new int[n + 1][k + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                memo[i][j] = -1;
+            }
+        }
+		return binomial(n, k, memo);
 	}
 
 	private static int binomial(int n, int k, int[][] memo) {
@@ -35,7 +39,7 @@ public class Binomial {
 		   	return 0;
 		}
 		// Another base case
-		if (n == 0 || k == 0) {
+		if (n == 0 || k == 0 || n == k) {
 		   	memo[n][k] = 1; 
 		   	return 1;
 		}
@@ -43,6 +47,3 @@ public class Binomial {
 		return memo[n][k];
 	}
 }
-
-
-
